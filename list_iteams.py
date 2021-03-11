1b1664e54b70726a1eb901fc9e31b506f6c9637a
raireddy733 <ravichandrareddy944@gmail.com>
2021-03-11 14:50:42 +0530

    commit

---
+++ list_iteams.py
@@ -0,0 +1,23 @@
+# names = ['rai', 'john', 'reddy', 'taylor', 'soha']
+# 
+# # for x in names:
+# #     print(x,end=' ')
+# # print(names , id (names))
+# print("only 1 to jump ", names[1::2])
+# print('all iteams in list', names[:])
+# 
+# 
+# print('modifies iteam', names)
+
+
+# l1 = (3,4,9,2,6,3,8)
+# l2 = (1,3,7,5,9,3,2)
+# l3 = l1 + l2
+# ic =l3.count(l2[5])
+# print(ic)
+
+s = 4
+def call():
+    s = 10
+call()
+print(s)
